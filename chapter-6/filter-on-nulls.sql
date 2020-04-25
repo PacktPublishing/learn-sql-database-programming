3fc639351e7bd057f91f6bbf8c67af52325cdb5d
sqlkitty <username@users.noreply.github.com>
2020-04-25 13:25:13 -0600

    code upload

--- chapter-6/filter-on-nulls.sql
+++ chapter-6/filter-on-nulls.sql
@@ -1,11 +1,11 @@
 #returns null values for g_defense
 USE lahmansbaseballdb;
- SELECT playerid, g_all, g_batting, g_defense
- FROM appearances
- WHERE g_defense IS NULL;
+SELECT playerid, g_all, g_batting, g_defense
+FROM appearances
+WHERE g_defense IS NULL;
  
 #returns not null values for g_defense 
 USE lahmansbaseballdb;
- SELECT playerid, g_all, g_batting, g_defense
- FROM appearances
- WHERE g_defense IS NOT NULL;
+SELECT playerid, g_all, g_batting, g_defense
+FROM appearances
+WHERE g_defense IS NOT NULL;
