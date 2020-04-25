a5e2426d77e351057178a4d6a7cafe6ec2215913
sqlkitty <username@users.noreply.github.com>
2020-04-25 16:41:16 -0600

    code upload

--- chapter-9/generated-columns.sql
+++ chapter-9/generated-columns.sql
@@ -6,6 +6,6 @@ AFTER lgID;
 
 USE lahmansbaseballdb;
 ALTER TABLE batting
-ADD COLUMN batavg DECIMAL(4,3) 
+ADD COLUMN batavgstored DECIMAL(4,3) 
 GENERATED ALWAYS AS (h/NULLIF(ab,0)) 
 STORED AFTER lgID;
--- chapter-9/statistical-functions.sql
+++ chapter-9/statistical-functions.sql
@@ -1,17 +1,10 @@
 USE lahmansbaseballdb; 
-SELECT COUNT(batavg) AS count,
-       SUM(batavg) AS sum,
-       AVG(batavg) AS mean,
-       STDDEV(batavg) AS 'stddev',
-       VARIANCE(batavg) AS 'variance',
-       MIN(batavg) AS minimum,
-       MAX(batavg) AS maximum
-FROM batting; 
-
-USE lahmansbaseballdb; 
-SELECT AVG(h) AS mean,
+SELECT COUNT(h) AS count,
+       SUM(h) AS sum,
+       AVG(h) AS mean,
        STDDEV(h) AS 'stddev',
        VARIANCE(h) AS 'variance',
        MIN(h) AS minimum,
        MAX(h) AS maximum
-FROM batting;
+FROM batting; 
+
