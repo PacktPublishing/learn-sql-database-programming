8e0d5d1ba4ea56bc8ba9729f679432e6ee60caa5
sqlkitty <username@users.noreply.github.com>
2020-05-16 10:46:21 -0600

    code upload

--- chapter-6/select-playerid-appearances.sql
+++
@@ -1,1 +0,0 @@
-SELECT playerid FROM lahmansbaseballdb.appearances;
--- chapter-6/where-clause.sql
+++
@@ -1,5 +0,0 @@
-#where clause 
-USE lahmansbaseballdb;
-SELECT playerid, g_all, g_batting, g_defense FROM appearances
-WHERE g_all > 40;
-
