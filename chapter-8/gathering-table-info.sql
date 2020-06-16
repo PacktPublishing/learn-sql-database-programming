505202c7f158c358aa6c7b6223a5ec9569f49469
utkarsha-Packt <utkarshak@packt.com>
2020-06-16 19:01:18 +0530

    Removed chapter-8 directory

--- chapter-8/gathering-table-info.sql
+++
@@ -1,9 +0,0 @@
-#in MySQL
-USE yourschema;
-describe managers;
- 
-SELECT * FROM information_schema.table_constraints
-WHERE table_name = 'managers'
-AND table_schema = 'yourschema';
-
-SHOW CREATE TABLE managers;
